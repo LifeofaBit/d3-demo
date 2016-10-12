21d9e05be31d8742525f8207ff89c7a34b697440
Matt Downs <matthewrdowns@gmail.com>
2016-10-12 08:44:09 -0400

    initial commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import render_template, flash, redirect
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('index.html')
+
+@app.route('/intro')
+def intro():
+	return render_template('intro.html')
+
+@app.route('/mon')
+def mon():
+	return render_template('mon.html', title="Pokemon Data")
---
+++ run.py
@@ -0,0 +1,3 @@
+#!/Users/mdowns/anaconda/bin/python
+from app import app
+app.run(debug=True)
